00e8762edfa743d6c8dbaaedaa375092ebc363f9
Eng Omar <engomaralryan@gmail.com>
2024-07-19 12:25:27 +0300

    update the board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,13 +7,24 @@ const initialBoard = [
 ];
 
 const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState(initialBoard);
+  function handleSelectSquare(rawIndex, colIndex) {
+    setGameBoard((prevBoard) => {
+      const updateBoard = [...prevBoard.map((innerArray) => [...innerArray])];
+      updateBoard[rawIndex][colIndex] = "X";
+      return updateBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
       {initialBoard.map((raw, rawIndex) => (
         <li key={rawIndex}>
           {raw.map((playerSymbol, colIndex) => (
             <ol key={colIndex}>
-              <button>{playerSymbol}</button>
+              <button onClick={() => handleSelectSquare(rawIndex, colIndex)}>
+                {playerSymbol}
+              </button>
             </ol>
           ))}
         </li>
