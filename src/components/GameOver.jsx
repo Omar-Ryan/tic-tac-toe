1fa9e1b880fecfcb1eee9b15d2f1d72ccd6cbd8f
Eng Omar <engomaralryan@gmail.com>
2024-07-26 23:49:41 +0300

    update part of gameover

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const GameOver = () => {
-  return <div>GameOver</div>;
+  return (
+    <div
+      style={{
+        width: "50%",
+        height: "30%",
+        border: "2px solid red",
+        borderRadius: "10px",
+      }}
+    >
+      <h1>GameOver</h1>
+      <p>Good luck next time</p>
+      <button>Once Again</button>
+    </div>
+  );
 };
 
 export default GameOver;
